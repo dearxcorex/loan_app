b73e0c3e9ad742ec46eba9f11a0502ce4fb7c68f
dearxoasis <46089780+dearxcorex@users.noreply.github.com>
2023-07-04 11:29:44 +0700

    Update firebase.tsx

--- firebase.tsx
+++ firebase.tsx
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAO0sc1AwDrrEZoJI9CnwLqMRiCOUc9ApQ",
-  authDomain: "loanapp-b961c.firebaseapp.com",
-  projectId: "loanapp-b961c",
-  storageBucket: "loanapp-b961c.appspot.com",
-  messagingSenderId: "28055878135",
-  appId: "1:28055878135:web:edd06b718726e5ccc116f2",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 // Initialize Firebase
