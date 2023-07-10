9faf799dd0e1112d89383cc971a137c6bce172d3
Deardevx <deardevx@gmail.com>
2023-07-11 00:28:32 +0700

    TryTonavigateInputPage

--- App.tsx
+++ App.tsx
@@ -1,74 +1,98 @@
 import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+
 import { Provider as PaperProvider } from "react-native-paper";
 import { Dimensions } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Header, Icon } from "@rneui/base";
-
+import { StackNavigationProp } from "@react-navigation/stack";
+import { Button } from "react-native-paper";
 import UserLogin from "./components/UserLogin";
 import Balance from "./components/Balance";
 import DetailsScreen from "./components/Details";
 import CreateButtonAdd from "./components/Addbutton/CreateButtonAdd";
 import InputPage from "./components/Addbutton/inputPage";
+import {
+  DrawerContentComponentProps,
+  createDrawerNavigator,
+} from "@react-navigation/drawer";
+import { Drawer } from "react-native-paper";
 
 //menu bar
-import { createDrawerNavigator } from "@react-navigation/drawer";
+
 import {
+  CompositeNavigationProp,
   NavigationContainer,
-  DrawerActions,
   useNavigation,
 } from "@react-navigation/native";
 
 const screenWidth = Dimensions.get("window").width;
 const Stack = createStackNavigator();
-const Drawer = createDrawerNavigator();
-interface HeaderStyleProps {
-  navigation: any; // You may want to replace 'any' with the correct type
-}
-//menu bar
-// const DrawerNavigator: React.FC = () => {
-//   return (
-//     <Drawer.Navigator>
-//       <Drawer.Screen name="Home" component={StackNavigator} />
-//       <Drawer.Screen name="add" component={CreateButtonAdd} />
-//     </Drawer.Navigator>
-//   );
-// };
+const DrawerNavigator = createDrawerNavigator();
 
-const HomeScreen: React.FC = () => {
-  console.log("HomeScreen");
+type RootStackParamList = {
+  InputPage: undefined;
+};
+
+type InputPageNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  "InputPage"
+>;
+
+interface HeaderStyleProps {
+  navigation: (value: string) => void;
+}
+
+const handleAddPress = () => {
+  const navigation = useNavigation<InputPageNavigationProp>();
+  navigation.navigate("InputPage");
+};
+const DrawerContent: React.FC<DrawerContentComponentProps> = () => {
+  // const handleAddPress = (navigation: any) => {
+  //   navigation.navigate("InputPage");
+  // };
+  console.log("DrawerComponent");
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen!!</Text>
+    <View>
+      <Drawer.Item
+        style={{ backgroundColor: "#fff", width: screenWidth * 0.3 }}
+        icon="star"
+        label="Add"
+        onPress={handleAddPress}
+      />
     </View>
   );
 };
 
-const HeaderStyle: React.FC<HeaderStyleProps> = ({ navigation }) => {
-  const openDrawer = () => {
-    navigation.dispatch(DrawerActions.openDrawer());
-  };
+// style drawer content
+const MyDrawer = () => {
   return (
-    <Header
-      barStyle="default"
-      centerComponent={{
-        text: "MY LOAN",
-        style: { color: "#fff" },
-      }}
-      containerStyle={{ width: screenWidth }}
-      leftComponent={{
-        icon: "menu",
-        color: "#fff",
-        onPress: openDrawer,
-      }}
-      leftContainerStyle={{}}
-      linearGradientProps={{}}
-      placement="center"
-      rightComponent={{ icon: "home", color: "#fff" }}
-    />
+    <DrawerNavigator.Navigator
+      drawerContent={(props) => (
+        <View style={styles.drawerContent}>
+          <DrawerContent {...props} />
+        </View>
+      )}
+    >
+      <DrawerNavigator.Screen name="Home" component={StackNavigator} />
+    </DrawerNavigator.Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  drawerContent: {
+    flex: 1,
+    paddingVertical: 30,
+
+    width: screenWidth * 0.7,
+    marginTop: 50, // Adjust the marginTop value as needed
+    marginBottom: 5, // Adjust the marginBottom value as needed
+    marginRight: 5, // Adjust the ma
+  },
+  drawernavigator: {
+    flex: 1,
+    width: screenWidth * 0.6,
+  },
+});
 
 //route
 const StackNavigator: React.FC<HeaderStyleProps> = ({ navigation }) => {
@@ -77,9 +101,7 @@ const StackNavigator: React.FC<HeaderStyleProps> = ({ navigation }) => {
       <Stack.Screen
         name="Balance"
         component={Balance}
-        options={{
-          header: () => <HeaderStyle navigation={navigation} />, // use it as a custom header
-        }}
+        options={{ headerShown: false }}
       />
       <Stack.Screen name="Details" component={DetailsScreen} />
       <Stack.Screen name="inputPage" component={InputPage} />
@@ -87,6 +109,16 @@ const StackNavigator: React.FC<HeaderStyleProps> = ({ navigation }) => {
   );
 };
 
+// const styles = StyleSheet.create({
+//   drawer: {
+//     backgroundColor: '#f0f0f0',
+//     width: 200, // Customize the width of the drawer
+//   },
+//   drawerContent: {
+//     flex: 1,
+//     paddingVertical: 20,
+//   },
+// });
 const Mycomponent: React.FC = () => {
   const [isLogin, setIsLogin] = useState(false);
 
@@ -96,9 +128,11 @@ const Mycomponent: React.FC = () => {
         <Stack.Navigator>
           {isLogin ? (
             <Stack.Screen
-              name="x"
-              component={StackNavigator}
-              options={{ headerShown: false }}
+              name="Home_2"
+              component={MyDrawer}
+              options={{
+                headerShown: false,
+              }}
             />
           ) : (
             <Stack.Screen name="Userlogin">
